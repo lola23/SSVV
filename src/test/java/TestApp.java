9ad26dbd119f85ced5004348ded267de898e1a1b
Lola <chiralarisaandreea@gmail.com>
2021-03-15 12:50:11 +0200

    Initial commit

---
+++ src/test/java/TestApp.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class TestApp {
+
+    @Test
+    public void test1() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void test2() {
+        assertFalse(false);
+    }
+
+}
